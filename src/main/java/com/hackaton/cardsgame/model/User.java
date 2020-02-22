36e1317f55364b1ecb7dbbfdfe93f453465078c1
Ledion Spaho <ledio5485@gmail.com>
2020-02-22 14:08:14 +0100

    login functionality

---
+++ src/main/java/com/hackaton/cardsgame/model/User.java
@@ -0,0 +1,20 @@
+package com.hackaton.cardsgame.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID uuid;
+    @Column
+    private String name;
+}
