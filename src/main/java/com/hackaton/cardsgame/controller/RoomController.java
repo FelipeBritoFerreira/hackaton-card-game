6b32e615a077d5204450a1956f3a715d7c8d38df
felipebrito <felipe.brito@audibene.de>
2020-02-22 13:34:48 +0100

    002

---
+++ src/main/java/com/hackaton/cardsgame/controller/RoomController.java
@@ -0,0 +1,24 @@
+package com.hackaton.cardsgame.controller;
+
+import com.hackaton.cardsgame.model.Room;
+import com.hackaton.cardsgame.service.RoomService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/room")
+@AllArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+
+}
---
+++ src/main/java/com/hackaton/cardsgame/model/Room.java
@@ -0,0 +1,22 @@
+package com.hackaton.cardsgame.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+}
