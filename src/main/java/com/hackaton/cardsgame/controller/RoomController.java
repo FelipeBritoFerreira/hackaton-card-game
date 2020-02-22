afa5b4baa2c00a3f641c94e2a62cd35f72faf59a
felipebrito <felipe.brito@audibene.de>
2020-02-22 14:14:36 +0100

    002

--- src/main/java/com/hackaton/cardsgame/controller/RoomController.java
+++ src/main/java/com/hackaton/cardsgame/controller/RoomController.java
@@ -4,6 +4,8 @@ import com.hackaton.cardsgame.model.Room;
 import com.hackaton.cardsgame.service.RoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class RoomController {
     @GetMapping
     public List<Room> getAllRooms() {
         return roomService.getAllRooms();
+    }
+
+    @PostMapping(value = "/save", consumes = "application/json")
+    public void save(@RequestBody Room room) {
+        roomService.save(room);
     }
 
 }
