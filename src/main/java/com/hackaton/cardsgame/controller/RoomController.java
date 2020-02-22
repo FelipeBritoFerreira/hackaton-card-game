35ee762b3536f22249b49532fa5c4861b9e90bbe
felipebrito <felipe.brito@audibene.de>
2020-02-22 15:34:12 +0100

    002

--- src/main/java/com/hackaton/cardsgame/controller/RoomController.java
+++ src/main/java/com/hackaton/cardsgame/controller/RoomController.java
@@ -1,11 +1,14 @@
 package com.hackaton.cardsgame.controller;
 
 import com.hackaton.cardsgame.model.Room;
+import com.hackaton.cardsgame.model.User;
 import com.hackaton.cardsgame.repository.RoomRepository;
+import com.hackaton.cardsgame.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,7 @@ import java.util.UUID;
 public class RoomController {
 
     private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
 
     @GetMapping
     public List<Room> getAllRooms() {
@@ -32,6 +36,14 @@ public class RoomController {
 
     @PostMapping
     public Room create(@RequestBody Room room) {
+        return roomRepository.save(room);
+    }
+
+    @PutMapping(value = "{uuid}/users/{userId}")
+    public Room addUser(@PathVariable UUID uuid, @PathVariable UUID userId) {
+        User user = userRepository.getOne(userId);
+        Room room = roomRepository.getOne(uuid);
+        room.getUsers().add(user);
         return roomRepository.save(room);
     }
 
